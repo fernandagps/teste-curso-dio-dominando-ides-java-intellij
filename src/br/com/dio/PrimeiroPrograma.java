55d211023bdfb114e987356945509a190affd172
Fernanda Paiola <fernandapaiola6@gmail.com>
2022-07-19 13:21:24 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,18 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+        /*int a = 5;
+        int b = 3;
+        System.out.println("Resultado da soma" + (a+b) );*/
+        System.out.println("Olá Chile");
+        System.out.println("Olá Pará");
+    }
+}
